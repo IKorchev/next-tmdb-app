8fdb3fefbdab77aa0d9ce9ce71bd1a9fc23f55cc
Korchev <Korchev94@gmail.com>
2021-08-02 19:09:34 +0100

    added movie route page and component, utils and more

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import Genres from "../components/Genres"
-import Navbar from "../components/Navbar"
-
-const Home = () => {
+import Results from "../components/Results"
+import requests from "../utils/requests"
+const Home = ({ jsonResult, genre }) => {
   return (
-    <div>
-      <Navbar />
+    <>
       <Genres />
-    </div>
+      <Results results={jsonResult.results} genre={genre} />
+    </>
   )
 }
 export default Home
+
+export const getServerSideProps = async (context) => {
+  const genre = context.query.genre
+  const result = await fetch(
+    `https://api.themoviedb.org/3${requests[genre]?.url || requests.trending.url}`
+  )
+  const jsonResult = await result.json()
+  return {
+    props: {
+      jsonResult,
+      genre: genre || "Trending",
+    },
+  }
+}
